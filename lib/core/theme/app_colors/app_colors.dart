98fc1d1f62188712323332e659cb339f1571ce75
Ahmet Islam Oktay <islamoktay34@gmail.com>
2022-03-05 18:51:38 +0300

    aaa

--- lib/core/theme/app_colors/app_colors.dart
+++ lib/core/theme/app_colors/app_colors.dart
@@ -4,7 +4,7 @@ class AppColors {
   static const Color appBarColor = Colors.white;
   static const Color scaffoldBackgroundColor = Color(0xFFF5F9FC);
   static const Color primaryColor = Colors.black;
-  static final Color orangeColor = Color(0xFFF38262);
+  static const Color orangeColor = Color(0xFFF38262);
   static const Color borderAndDividerColor = Color(0xFFf2f2fc);
   static const Color textColor = Color(0xFF424242);
   static const Color yellowColor = Color(0xFFffbc41);
